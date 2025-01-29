5d543507e882c0e4014cd1cdb3946a64c9c9d09f
Andrew Larkworthy <andrew@larkworthy.org>
2025-01-28 22:34:22 -0700

    audio

--- modules/nixos/audio/default.nix
+++ modules/nixos/audio/default.nix
@@ -22,6 +22,24 @@ in
       alsa.support32Bit = true;
       pulse.enable = true;
       #jack.enable = true; #for JACK
-   };
+			extraConfig.pipewire-pulse."92-low-latency" = {
+  context.modules = [
+    {
+      name = "libpipewire-module-protocol-pulse";
+      args = {
+        pulse.min.req = "32/48000";
+        pulse.default.req = "32/48000";
+        pulse.max.req = "32/48000";
+        pulse.min.quantum = "32/48000";
+        pulse.max.quantum = "32/48000";
+      };
+    }
+  ];
+  stream.properties = {
+    node.latency = "32/48000";
+    resample.quality = 1;
   };
+};
+  	};
+	};
 }
