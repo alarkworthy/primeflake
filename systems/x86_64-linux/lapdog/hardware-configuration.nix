41f3362ff3673591f97092fd22958de9cb97ce9f
Andrew Larkworthy <andrew@larkworthy.org>
2025-03-26 16:07:01 -0600

    added swap file to laptop

--- systems/x86_64-linux/lapdog/hardware-configuration.nix
+++ systems/x86_64-linux/lapdog/hardware-configuration.nix
@@ -89,7 +89,12 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+		{
+			device = "/swap/swapfile";
+			#randomEncryption.enable = true;
+		}
+	];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
