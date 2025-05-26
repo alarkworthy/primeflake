3e35f505097defdfc762cf150671e8aed28269f4
Andrew Larkworthy <andrew@larkworthy.org>
2025-05-26 13:49:37 -0400

    override

--- flake.nix
+++ flake.nix
@@ -123,8 +123,17 @@
         nixpkgs-xr.overlays.default
         (final: prev:
           {
-            wlx-overlay-s = prev.wlx-overlay-s.overrideAttrs (prevAttrs: { 
+            wlx-overlay-s = prev.wlx-overlay-s.overrideAttrs (prevAttrs: {
               postPatch = nixpkgs.legacyPackages."x86_64-linux".wlx-overlay-s.postPatch;
+              });
+            xrizer = nixpkgs.legacyPackages."x86_64-linux".xrizer.overrideAttrs (prevAttrs: {
+              src = prev.fetchFromGitHub {
+                  owner = "RinLovesYou";
+                  repo = "xrizer";
+                  rev = "f491eddd0d9839d85dbb773f61bd1096d5b004ef";
+                  hash = "sha256-12M7rkTMbIwNY56Jc36nC08owVSPOr1eBu0xpJxikdw=";
+                };
+              doCheck = false;
               });
           }
         )
