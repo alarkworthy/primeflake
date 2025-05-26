147aba1edda94c286ce654645b303faead438da2
Andrew Larkworthy <andrew@larkworthy.org>
2025-05-26 14:27:39 -0400

    removed flags from monadoEnvironment

--- modules/nixos/wivr/default.nix
+++ modules/nixos/wivr/default.nix
@@ -21,14 +21,14 @@
       defaultRuntime = true;
       openFirewall = true;
       monadoEnvironment = {
-        XRT_COMPOSITOR_COMPUTE = "1";
+        # XRT_COMPOSITOR_COMPUTE = "1";
         #IPC_EXIT_ON_DISCONNECT = "0";
         #XRT_COMPOSITOR_LOG = "debug";
         #XRT_PRINT_OPTIONS = "1";
-        U_PACING_COMP_MIN_TIME_MS = "5";
+        # U_PACING_COMP_MIN_TIME_MS = "5";
         #U_PACING_APP_USE_MIN_FRAME_PERIOD = "1";
         #XRT_COMPOSITOR_DEFAULT_FRAMERATE="120";
-        AMD_VULKAN_ICD = "RADV";
+        #AMD_VULKAN_ICD = "RADV";
         #XRT_CURATED_GUI = "1";
         #XRT_DEBUG_GUI = "1";
       };
